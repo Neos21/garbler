449d0f4bc999eecdeb409bcf30c317bef27ccb86
Neos21 <neos21@gmail.com>
2020-02-26 17:45:16 +0900

    Modify

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// global is not defined 対応
+(window as any).global = window;
